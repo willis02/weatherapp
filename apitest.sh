8f16f284d825750618d0cb3316aa629bf28c253e
Will <willis02@yahoo.com>
2022-02-25 17:05:01 +1300

    Add town weather

--- apitest.sh
+++ apitest.sh
@@ -1,1 +1,7 @@
-curl -svq 'https://api.openweathermap.org/data/2.5/weather?q=Auckland&appid=53ceb875c177aab8f9e62a7374c3e5fc' | jq .
+echo "API_KEY is $API_KEY"
+source .env
+echo "API_KEY is $API_KEY"
+# Auckland town wearher
+curl -svq "https://api.openweathermap.org/data/2.5/weather?q=Auckland&appid=$API_KEY" | jq .
+# Lat/Lon weather
+curl -sq "https://api.openweathermap.org/data/2.5/weather?lat=-36.877278&lon=174.764160&appid=$API_KEY" | jq .
